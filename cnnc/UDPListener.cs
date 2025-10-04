6ef315b0b89f798f3781bed3f5930fbccfe9bc35
MatteoXAntoz <108668752+MatteoXAntoz@users.noreply.github.com>
2025-10-04 22:59:45 +0200

    init

---
+++ cnnc/UDPListener.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace cnnc
+{
+    public class UDPListener
+    {
+        private const int listenPort = 5000;
+        static Writing writing;
+
+        static void Main()
+        {
+            writing = new Writing();
+         
+          
+
+            Thread thread = new Thread(StartListener);
+
+            thread.Start();
+        }
+
+        static void StartListener()
+        {
+            UdpClient listener = new UdpClient(listenPort);
+            IPAddress ipAddress = IPAddress.Parse("192.168.0.105");
+            IPEndPoint groupEP = new IPEndPoint(ipAddress, listenPort);
+
+            Console.WriteLine("UDP Listener gestartet...");
+
+            while (true)
+            {
+                byte[] bytes = listener.Receive(ref groupEP);
+                Debug.WriteLine($"Received broadcast from {groupEP} :");
+                Debug.WriteLine($" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}");
+
+                String[] message = Encoding.ASCII.GetString(bytes, 0, bytes.Length).Split(";");
+                int xPos = int.Parse(message[0]);
+                int yPos = int.Parse(message[1]);
+                float pressure = float.Parse(message[2], CultureInfo.InvariantCulture);
+
+                writing.simulatePenTap(xPos, yPos, pressure);
+            }
+
+            try
+            {
+               
+            }
+            catch (SocketException e)
+            {
+                Debug.WriteLine(e);
+            }
+            finally
+            {
+                listener.Close();
+            }
+        }
+
+       
+    }
+
+}
---
+++ cnnc/Writing.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using Windows.UI.Input.Preview.Injection;
+
+namespace cnnc
+{
+    public class Writing
+    {
+        private InputInjector injector;
+
+        public Writing()
+        {
+            injector = InputInjector.TryCreate();
+            if (injector == null)
+            {
+                Debug.WriteLine("InputInjector konnte nicht erstellt werden.");
+            }
+
+           
+            
+        }
+
+        public void simulatePenTap(int x, int y, float pressure)
+        {
+
+            InjectedInputPenInfo injectedInputPenInfo = new InjectedInputPenInfo()
+            {
+                PointerInfo = new InjectedInputPointerInfo()
+                {
+                    PointerOptions = InjectedInputPointerOptions.InContact ,
+                    PixelLocation = new InjectedInputPoint()
+                    {
+                        PositionX = x,
+                        PositionY = y   
+                    }
+                    
+                },
+
+
+                Pressure = pressure,
+                PenParameters = InjectedInputPenParameters.Pressure
+
+
+            };
+
+            injector.InjectPenInput(injectedInputPenInfo);
+
+
+
+
+
+
+        }
+    }
+}
